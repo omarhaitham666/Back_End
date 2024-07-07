f12a0b50ad40f7b7a09b4abcf6e218a9ef24084c
omarhaitham666 <162066262+omarhaitham666@users.noreply.github.com>
2024-07-07 21:38:13 +0300

    Add files via upload

---
+++ thirdApp/index.php
@@ -0,0 +1,14 @@
+<?php
+$var="PHP Tutorial"
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "<h3>$var</h3>"?>
+</body>
+</html>
